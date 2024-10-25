24a36064539357b0351038ec77ec54c75b472aa5
Akos Fabian <fabian.akos@gmail.com>
2024-10-25 15:23:47 +0200

    safe fake data on every update

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,10 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 import Json.Decode as D
+import Json.Encode as E
 
 
 type alias Model =
@@ -14,12 +15,15 @@ type Msg
     = Increment
 
 
+port saveToLocalStorage : String -> Cmd msg
+
+
 main : Program String Model Msg
 main =
     Browser.element
         { init = init
         , view = view
-        , update = update
+        , update = updateWithStorage
         , subscriptions = subscriptions
         }
 
@@ -27,6 +31,11 @@ main =
 flagDecoder : D.Decoder Int
 flagDecoder =
     D.field "age" D.int
+
+
+encode : Model -> E.Value
+encode model =
+    E.object [ ( "age", E.int model ) ]
 
 
 init : String -> ( Model, Cmd Msg )
@@ -48,7 +57,19 @@ view model =
     div []
         [ text (String.fromInt model)
         , button [ onClick Increment ] [ text "Increment" ]
+
+        -- , button [ onClick saveToLocalStorage ] [ text "Save" ]
         ]
+
+
+updateWithStorage : Msg -> Model -> ( Model, Cmd Msg )
+updateWithStorage msg model =
+    let
+        ( newModel, cmds ) =
+            update msg model
+    in
+    -- ( newModel, Cmd.batch [ saveToLocalStorage (encode model), cmds ] )
+    ( newModel, Cmd.batch [ saveToLocalStorage "{\"age\": 23}", cmds ] )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
